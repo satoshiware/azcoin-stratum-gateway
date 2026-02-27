3f85fd8ccba68e8a5856ca3d15d8efc9a34f2fb4
bcraypo <bcraypo@gmail.com>
2026-02-27 11:19:11 -0700

    v0.1.3: fixing gateway

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,7 +1,7 @@
 Set-Location C:\AZCoin\01_PROJECTS\azcoin-stratum-gateway
 git status
 git add -A
-git commit -m "v0.1.3: testing job store"
+git commit -m "v0.1.3: fixing gateway"
 git tag v0.1.3 -m "v0.1.3"
 git push origin main
 git push origin v0.1.3
